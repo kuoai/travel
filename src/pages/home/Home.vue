c0e30d4806cb912c88e3d691a31f41eded099f4d
heminhui <1334508254@qq.com>
2019-03-08 14:55:27 +0800

    项目初始化

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        hello world
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    name: 'Home',
+    data() {
+        return {
+        }
+    },
+}
+</script>
+
+<style scoped>
+</style>
