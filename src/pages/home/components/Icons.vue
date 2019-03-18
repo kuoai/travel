6fddc309366efa039f3616fe7496bd657f351fd8
heminhui <1334508254@qq.com>
2019-03-18 16:44:15 +0800

    首页组件

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -79,34 +79,35 @@ export default {
 .icons >>> .swiper-container
     height 0
     padding-bottom 50%
-.icon
-    width 25%
-    height 0
-    float left
-    padding-bottom 25%
-    overflow hidden
-    position relative
-    .icon-img
-        position absolute
-        top 0px
-        left 0px
-        right 0px
-        bottom 0.44rem
-        box-sizing border-box
-        padding 0.03rem
-        .icon-img-content
-            height 100%
-            display block
-            margin 0 auto
-    .icon-desc
-        position absolute
-        left 0
-        right 0
-        bottom 0
-        line-height 0.44rem
-        height 0.44rem
-        text-align center
-        color $darkTextColor
-        ellipisi()
-        
+.icons
+    margin-top 0.1rem
+    .icon
+        width 25%
+        height 0
+        float left
+        padding-bottom 25%
+        overflow hidden
+        position relative
+        .icon-img
+            position absolute
+            top 0px
+            left 0px
+            right 0px
+            bottom 0.44rem
+            box-sizing border-box
+            padding 0.03rem
+            .icon-img-content
+                height 100%
+                display block
+                margin 0 auto
+        .icon-desc
+            position absolute
+            left 0
+            right 0
+            bottom 0
+            line-height 0.44rem
+            height 0.44rem
+            text-align center
+            color $darkTextColor
+            ellipisi()
 </style>
